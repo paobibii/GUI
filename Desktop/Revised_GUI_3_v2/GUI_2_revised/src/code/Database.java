fd7c0e627a511221a3532bec456acba3f301f518
paobibii <napat.t@ku.th>
2015-12-06 01:14:29 +0700

    Update Database.java

--- Desktop/Revised_GUI_3_v2/GUI_2_revised/src/code/Database.java
+++ Desktop/Revised_GUI_3_v2/GUI_2_revised/src/code/Database.java
@@ -1,9 +1,11 @@
 package code;
 import java.util.*;
 public class Database {
-
 	public ArrayList<String> member ;
 	public ArrayList<String> member_Lastname ;
+	public ArrayList<Integer> member_age ;
+	public ArrayList<String> gender ;
+	
 	public ArrayList<String> getMember_Lastname() {
 		return member_Lastname;
 	}
@@ -22,15 +24,26 @@ public class Database {
 	public void setGender(ArrayList<String> gender) {
 		this.gender = gender;
 	}
-	public ArrayList<Integer> member_age ;
-	public ArrayList<String> gender ;
-	public void addMember(String mem){
-    	member.add(mem);
-    }
 	public ArrayList<String> getMember() {
 		return member;
 	}
 	public void setMember(ArrayList<String> member) {
 		this.member = member;
 	}
+	public void addMember(String mem){
+    		member.add(mem);
+   	}
+   	public void addMemberLastname(String lastname){
+   		member_Lastname.add(lastname);
+   	}
+   	public void addMemberAge(int age){
+   		member_age.add(age) ;
+   	}
+   	public void addGender(String gender){
+   		this.gender.add(gender) ;
+   	}
+   	public void removeMember(String mem){
+   		member.remove(mem);
+   	}
+
 }
